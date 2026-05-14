10f76cd83a161a01694f36af52b698c33c323fca
samprit004 <sampritdas2004@gmail.com>
2026-05-15 00:48:30 +0530

    terminal window added and implemented a hoc to manage all the windows

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,50 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import useWindowStore from '#store/Window.js'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
+const WindowWrapper = (Component, windowKey) => {
+
+    const Wrapped = (props) => {
+        const {focusWindow, windows} = useWindowStore();
+        const {isOpen, zIndex} = windows[windowKey];
+        const ref = useRef(null);
+
+        useGSAP(() => {
+            const el = ref.current;
+            if(!el || !isOpen) return;
+            el.style.display = 'block';
+
+            gsap.fromTo(
+                el, 
+                { scale: 0.8, opacity: 0 , y: 40},
+                { scale: 1, opacity: 1, y: 0, duration: 0.1, ease: "power3.Out"});
+        },[isOpen])
+
+        useLayoutEffect(() => {
+            const el = ref.current;
+            if(!el) return;
+            el.style.display = isOpen ? 'block' : 'none';
+        },[isOpen])
+
+        useGSAP(() => {
+            const el = ref.current;
+            if(!el) return;
+
+            const [instance] = Draggable.create(el, {onpress: () => focusWindow(windowKey)});
+            return () => instance.kill();
+        },[])
+
+        return( <section id={windowKey} ref={ref} style={{zIndex, display: 'none'}} className="absolute">
+            <Component {...props} />
+        </section>
+        )
+    }
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || 'Component'})`;
+
+    return Wrapped;
+}
+
+export default WindowWrapper
